deab403cce4d3939a444c993dd04c8f89d78bb27
Diego Molina <42214819+steelpwner@users.noreply.github.com>
2020-03-05 13:41:41 -0500

    sum of multiples

---
+++ sum-of-multiples/sum_of_multiples.py
@@ -0,0 +1,12 @@
+def sum_of_multiples(limit, multiples):
+    result = []
+    if (0 in multiples and len(multiples)==1):
+    	return 0
+    elif (0 in multiples):
+    	multiples.remove(0)
+    def filtro(n):
+    	for i in multiples:
+    		if (n % i == 0):
+    			return True
+    	return False
+    return sum(filter(filtro,[x for x in range(limit)]))
