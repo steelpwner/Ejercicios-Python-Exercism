d204ef5356acb9aeb145216bf82c313f6096a028
Diego Molina <42214819+steelpwner@users.noreply.github.com>
2020-03-05 02:03:47 -0500

    grains

---
+++ grains/grains.py
@@ -0,0 +1,7 @@
+def square(number):
+    if (number > 64 or number < 1):
+    	raise ValueError("Number not in range")
+    return 2 ** (number - 1)
+
+def total():
+    return sum([2 ** (x-1) for x in range(1,65)])
