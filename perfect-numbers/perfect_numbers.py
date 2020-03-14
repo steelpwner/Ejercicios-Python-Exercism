1755adddf0fa843658dfd3d67e499fff5780baf3
Diego Molina <42214819+steelpwner@users.noreply.github.com>
2020-03-14 12:52:32 -0500

    Pep8Perfect

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,5 +1,13 @@
 def classify(number):
-	if (number < 1):
-		raise ValueError("Number is less than 1")
-	suma = sum(filter(lambda x: number % x == 0, [x for x in range(1,int(number/2)+1)]))
-	return  "perfect" if suma == number else "abundant" if suma > number else "deficient"
+    if (number < 1):
+        raise ValueError("Number is less than 1")
+    numbers = [x for x in range(1, int(number/2)+1)]
+    suma = sum(filter(lambda x: number % x == 0, numbers))
+    out = ""
+    if suma == number:
+        out = "perfect"
+    elif suma > number:
+        out = "abundant"
+    else:
+        out = "deficient"
+    return out
